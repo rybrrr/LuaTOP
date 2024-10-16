2822ed761446882cc0ab98922e3d4cfcd4bdf639
rybrrr <kopejskat@gchd.cz>
2024-10-16 15:53:40 +0200

    Add, Find, FindMinimum

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Node uzlik = new Node(8);
+        
     }
 
     class Node
@@ -13,6 +14,67 @@ class Program
         {
             Value = value;
         }
-        public int Value { get; };
-        public Node Next { get; set };
+        public int Value { get; }
+        public Node Next { get; set; }
+    }
+
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public void Add(int value)
+        {
+            Node newHead = new Node(value);
+            if (Head != null)
+            {
+                newHead.Next = Head;
+            }
+            Head = newHead;
+        }
+
+        public bool Find(int value)
+        {
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                {
+                    return true;
+                }
+                currentNode = currentNode.Next;
+            }
+            return false;
+        }
+
+        public int FindMinimum()
+        {
+            // O(n)
+
+            int minimum;
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value < minimum | minimum == null)
+                {
+                    minimum = currentNode.Value;
+                }
+                currentNode = currentNode.Next;
+            }
+            return minimum;
+        }
+
+        public void Sort()
+        {
+
+        }
+    }
+
+    /*
+    1. Nalezení minima ve spojovém seznamu (30b)
+    2. Seřazení hodnot ve spojovém seznamu vzestupně (50b)
+    3. Destruktivní průnik dvou seznamů (70b)Destruktivní průnik znamená, že z prvků obou seznamů vytvoříte jeden seznam obsahující právě všechny společné prvky a to každý pouze jednou. Funkce nevytváří žádné nové prvky, ale používá pouze ty již existující.
+
+    Upozornění: V jednom seznamu se hodnoty mohou opakovat. Seznamy mohou být i prázdné.
+
+    BONUS (+10b): Určete časovou složitost všech vašich implementací (n je počet prvků v poli).
+    */
 }
