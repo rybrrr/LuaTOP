12abb10dc19a4bf9d5b14edd8029f13387a1086c
rybrrr <kopejskat@gchd.cz>
2024-10-29 17:59:56 +0100

    Update Program.cs

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -53,7 +53,7 @@ class Program
             Node currentNode = Head;
             while (currentNode != null)
             {
-                if (currentNode.Value < minimum | minimum == null)
+                if (minimum == null currentNode.Value < minimum)
                 {
                     minimum = currentNode.Value;
                 }
